d795e8e57d8be93bdc4e99f1b0ab334cdfd1498c
Jonathan Ong <jonathao@unix4.andrew.cmu.edu>
2015-09-27 14:43:31 -0400

    Added stubs for 4.4 & 4.6

---
+++ p2/user/progs/test_console.c
@@ -0,0 +1,40 @@
+#include <syscall.h>
+#include <simics.h>
+
+int main()
+{
+    /* Test getchar() */
+    // lprintf("Press a key...");
+    // lprintf("getchar(): %c", getchar());
+    
+    /* Test readline() */
+    // int len = 100;
+    // char buf[len];
+    // lprintf("readline(): %d", readline(len, buf));
+    // lprintf("buffer: %s", buf);
+    
+    /* Test print() */
+    // int len = 30;
+    // char *buf = "Hello this is a test message!\n";
+    // lprintf("print(): %d", print(len, buf));
+    
+    /* Test set_term_color() */
+    // int len = 30;
+    // char *buf = "Hello this is a test message!\n";
+    // print(len, buf);
+    // lprintf("set_term_color(0x24): %d", set_term_color(0x24));
+    // print(len, buf);
+    // lprintf("set_term_color(ERROR): %d", set_term_color(0x9F));
+    
+    /* Test set_cursor_pos() and get_cursor_pos() */
+    // int row, col;
+    // lprintf("get_cursor_pos(): %d", get_cursor_pos(&row, &col));
+    // lprintf("row: %d, col: %d", row, col);
+    // lprintf("set_cursor_pos(20, 20): %d", set_cursor_pos(20, 20));
+    // lprintf("get_cursor_pos(): %d", get_cursor_pos(&row, &col));
+    // lprintf("row: %d, col: %d", row, col);
+    
+    while(1){
+        continue;
+    }
+}
---
+++ p2/user/progs/test_thread.c
@@ -0,0 +1,41 @@
+#include <syscall.h>
+#include <simics.h>
+
+int main()
+{
+    /* Test gettid() */
+    // lprintf("gettid(): %d", gettid());
+    
+    /* Test yield() */
+    // lprintf("yield(-1): %d", yield(-1));
+    
+    /* Test deschedule() */
+    // int x = 1;
+    // lprintf("deschedule(&non-zero): %d", deschedule(&x));
+    // lprintf("deschedule(bad_pointer): %d", deschedule(0));
+    
+    /* Test make_runnable() */
+    // lprintf("make_runnable(scheduled): %d", make_runnable(gettid()));
+    
+    /* Test make_runnable() with deschedule() */
+    // TODO: Need to test with separate threads
+    // int y = 0;
+    // lprintf("deschedule(&zero): %d", deschedule(&y));
+    // lprintf("make_runnable(descheduled): %d", make_runnable(gettid()));
+    
+    /* Test get_ticks() */
+    // lprintf("get_ticks(): %u", get_ticks());
+    
+    /* Test sleep() */
+    // lprintf("sleep(-1): %d", sleep(-1));
+    // lprintf("get_ticks(): %u", get_ticks());
+    // lprintf("sleep(10): %d", sleep(10));
+    // lprintf("get_ticks(): %u", get_ticks());
+    
+    /* Test swexn() */
+    // TODO: Add test for software exception register/deregister
+    
+    while(1){
+        continue;
+    }
+}
