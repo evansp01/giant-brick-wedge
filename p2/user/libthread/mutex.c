13b1a8472d363ce578c99b8613a8ed8e8c050930
Evan Palmer <evansp01@gmail.com>
2015-10-07 14:49:24 -0400

    added comments, fairness for aquire

--- p2/user/libthread/mutex.c
+++ p2/user/libthread/mutex.c
@@ -1,10 +1,13 @@
 /** @file mutex.c
  *  @brief An implementation of mutexes
  *
- *  NOTE: could use fetch and add for ticket locks to ensure real fairness
+ *  This implementation of mutexes tracks an owner and the number of threads
+ *  waiting to allow for some fairness guarantees.
  *
  *  @author Evan Palmer (esp)
+ *  @bug No known bugs
  **/
+
 #include <mutex.h>
 #include <thread.h>
 #include <syscall.h>
@@ -16,6 +19,13 @@
 #define UNLOCKED 0
 #define UNSPECIFIED -1
 
+/** @brief Initialize mutex
+ *  This initializes a mutex allowing it to be locked. Mutexes are initialized
+ *  to the unlocked state.
+ *
+ *  @param mp The mutex to initialize
+ *  @return void
+ **/
 int mutex_init(mutex_t* mp)
 {
     //unlocked and nobody owns
@@ -25,6 +35,13 @@ int mutex_init(mutex_t* mp)
     return 0;
 }
 
+/** @brief Destory a mutex
+ *  Mutexes cannot be operated on after being destroyed unless init is called
+ *  again. It is illegal to destroy a locked mutex
+ *
+ *  @param mp The mutex to destroy
+ *  @return void
+ **/
 void mutex_destroy(mutex_t* mp)
 {
     //lock the mutex
@@ -35,12 +52,18 @@ void mutex_destroy(mutex_t* mp)
     mp->owner = UNSPECIFIED;
 }
 
+/** @brief Lock a mutex
+ *  Blocks until the lock for this mutex is aquired
+ *
+ *  @param mp The mutex to lock
+ *  @return void
+ **/
 void mutex_lock(mutex_t* mp)
 {
     //might as well get the tid, we'll need it later
     int thread_id = thr_getid();
-    //let's see if we can get the lock immediately
-    if (atomic_xchg(&mp->lock, LOCKED) == UNLOCKED) {
+    //let's see if we can get the lock immediately (provided nobody is waiting)
+    if (mp->waiting == 0 && atomic_xchg(&mp->lock, LOCKED) == UNLOCKED) {
         mp->owner = thread_id;
         return;
     }
@@ -59,6 +82,13 @@ void mutex_lock(mutex_t* mp)
     mp->owner = thread_id;
 }
 
+/** @brief Unlock a mutex
+ *  Unlocks the given mutex. It is illegal for a thread other than the thread
+ *  which called lock to unlock a mutex
+ *
+ *  @param mp The mutex to unlock
+ *  @return void
+ **/
 void mutex_unlock(mutex_t* mp)
 {
     if (mp->lock == LOCKED && mp->owner == UNSPECIFIED) {
