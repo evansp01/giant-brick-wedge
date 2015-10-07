7754ee89ee2d9ab6eea03fc6f8abb814981dca1c
Evan Palmer <evansp01@gmail.com>
2015-10-07 15:06:04 -0400

    added comments to exit.c

--- p2/user/libthread/exit.c
+++ p2/user/libthread/exit.c
@@ -5,11 +5,21 @@
 
 static int thread_initialized = 0;
 
+/** @brief Instruct exit to call thr_exit instead of exiting normally
+ *  @return void
+ **/
 void threaded_exit()
 {
     thread_initialized = 1;
 }
 
+/** @brief Links over the default exit method, and allows for threaded exit
+ *  This exit function will call thr_exit instead of set_status and vanish
+ *  if threaded_exit has been called.
+ *
+ *  @param status The exit status
+ *  @return Does not return
+ **/
 void exit(int status)
 {
     if (thread_initialized) {
