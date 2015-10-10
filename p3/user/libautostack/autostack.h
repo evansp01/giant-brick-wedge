8550198b2ede9ecebd7af90f613fe6696e8b62a3
Evan Palmer <evansp01@gmail.com>
2015-10-10 05:54:32 -0400

    added libautostack from p2

---
+++ p3/user/libautostack/autostack.h
@@ -0,0 +1,11 @@
+/** @file autostack.h
+ *  @brief Interface for autostack functions
+ *
+ *  @author Jonathan Ong (jonathao) and Evan Palmer (esp)
+ *  @bug No known bugs
+ **/
+#ifndef AUTOSTACK_H
+#define AUTOSTACK_H
+void install_threaded(void *thread_stack);
+void get_stack_bounds(void** stack_high, void** stack_low);
+#endif // AUTOSTACK_H
