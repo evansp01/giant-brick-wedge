9a419b916c988ab5a08b53d52c651b3731afab17
Evan Palmer <evansp01@gmail.com>
2015-10-16 15:14:57 -0400

    added page struct header

---
+++ p3/kern/page_structs.h
@@ -0,0 +1,15 @@
+#include <stdint.h>
+
+typedef struct {
+    uint32_t present: 1;             /* bit 0 */
+    uint32_t read_write: 1;          /* bit 1 */
+    uint32_t user_superuser: 1;      /* bit 2 */
+    uint32_t write_through: 1;       /* bit 3 */
+    uint32_t cache_disable: 1;       /* bit 4 */
+    uint32_t accessed: 1;            /* bit 5 */
+    uint32_t dirty: 1;               /* bit 6 */
+    uint32_t page_size: 1;           /* bit 7 */
+    uint32_t global: 1;              /* bit 8 */
+    uint32_t available: 3;           /* bit 9  - 11 */
+    uint32_t address: 20;            /* bit 12 - 32 */
+} entry_t;
