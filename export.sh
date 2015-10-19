dab8d0a97378a56d37a471bf13cc92ee8f7159a5
Evan Palmer <evansp01@gmail.com>
2015-10-19 01:35:48 -0400

    script is cool

--- export.sh
+++ export.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+function usage {
+    echo "./export localdir projectdir"
+    echo "    localdir - copy from (path from root of git repo)"
+    echo "    projectdir - copy to (path from myproj) "
+}
+
+if [ "$#" -ne 2 ]; then
+    usage
+    exit
+fi
+
 git_root=$(git rev-parse --show-toplevel)
 group_dir="/afs/cs.cmu.edu/academic/class/15410-f15-users/group-27"
-project="p3"
 
 up_to_date=$(git fetch -v --dry-run)
 
@@ -11,9 +21,9 @@ if [[ -n $up_to_date ]]; then
     exit
 fi
 
-git_project_dir="$git_root/$project"
-submit_project_dir="$group_dir/$project"
+git_project_dir="$git_root/$1"
+submit_project_dir="$group_dir/$2"
 
-rm -r "$submit_project_dir"/*
+rm -rf "$submit_project_dir"/*
 cd $git_project_dir && make veryclean > /dev/null
 cp -r "$git_project_dir"/* $submit_project_dir
