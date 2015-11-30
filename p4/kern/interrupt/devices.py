b5829b125710975d893a61c3d6872c397fff2daa
Evan Palmer <evansp01@gmail.com>
2015-11-30 16:25:17 -0500

    added python script to generate things

---
+++ p4/kern/interrupt/devices.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+
+things = range(32,256)
+
+
+for i in things:
+    print("EXCEPTION_ASM_WRAPPER_DEVICE {}".format(i))
+
+print()
+print()
+print()
+
+for i in things:
+    print()
+    print("/** @brief Wrapper for user interrupt {}".format(i))
+    print(" *  @return void")
+    print(" **/")
+    print("INT_ASM_H({});".format(i))
+
+
+print()
+print()
+print()
+
+for i in things:
+    print("set_idt_device(INT_ASM({}), {});".format(i, i))
