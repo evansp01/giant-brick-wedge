7f5fdca3710e6e117dff35d742b55784f804d8a1
Evan Palmer <evansp01@gmail.com>
2015-12-10 21:26:24 -0500

    added serial console comments

--- p4/user/progs/serial_console.c
+++ p4/user/progs/serial_console.c
@@ -26,6 +26,14 @@ struct {
     int index;
 } printer;
 
+/** @brief Send string from the print server to the console driver to be
+ *         printed
+ *
+ *  @param len The length of the message
+ *  @param suggest_id The udriv server which the serial server accepts print
+ *                    suggestions on
+ *  @return void
+ **/
 void print_message(int len, int suggest_id)
 {
     mutex_lock(&printer.mutex);
@@ -37,6 +45,10 @@ void print_message(int len, int suggest_id)
     mutex_unlock(&printer.mutex);
 }
 
+/** @brief Get a character from the buffer provided by the print server
+ *  @param c A pointer to the character which will be filled on success
+ *  @return A boolean integer indicating whether a character was obtained
+ **/
 int get_print_char(char* c)
 {
     int got = 0;
@@ -55,6 +67,9 @@ int get_print_char(char* c)
     return got;
 }
 
+/** @brief Initialize the serial console
+ *  @return void
+ **/
 void init_console()
 {
     int i;
@@ -66,6 +81,15 @@ void init_console()
     cond_init(&printer.cvar);
 }
 
+/** @brief Get any partial characters which must be printed before anything else
+ *
+ *  Certain characters like newline and backspace must be expanded so that they
+ *  render properly on the console. This function gets any characters produced
+ *  by the expansion of the most recent "real" character
+ *
+ *  @param c A pointer which will be filled with a charcter
+ *  @return A boolean integer indicating whether a character was obtained
+ **/
 int get_bs_char(char* c)
 {
     if (bs_buffer.bs_index != bs_buffer.bs_len) {
@@ -76,16 +100,24 @@ int get_bs_char(char* c)
     return 0;
 }
 
-int r_index(int i)
+/** @brief Get the index in the readline echo buffer
+ *  @param i The index to convert to a circular buffer index
+ **/
+int buffer_index(int i)
 {
     return (i + READLINE_MAX_LEN * 2) % (READLINE_MAX_LEN * 2);
 }
 
+/** @brief Echo readline characters to be printed
+ *  @param len The number of characters to send
+ *  @param buf The buffer containing the characters
+ *  @return 0
+ **/
 int send_to_print(int len, char* buf)
 {
     int i;
     for (i = 0; i < len; i++) {
-        int next = r_index(r_print.producer + 1);
+        int next = buffer_index(r_print.producer + 1);
         if (next != r_print.consumer) {
             r_print.buf[r_print.producer] = buf[i];
             r_print.producer = next;
@@ -94,16 +126,26 @@ int send_to_print(int len, char* buf)
     return 0;
 }
 
+/** @brief Poll characters which are to be echoed by readline
+ *  @param c A pointer to a polled character on success
+ *  @return a boolean integer indicating whether a character was polled
+ **/
 int poll_from_print(char* c)
 {
     if (r_print.consumer != r_print.producer) {
         *c = r_print.buf[r_print.consumer];
-        r_print.consumer = r_index(r_print.consumer + 1);
+        r_print.consumer = buffer_index(r_print.consumer + 1);
         return 1;
     }
     return 0;
 }
 
+/** @brief Get the next character to be printed by the console. Looks for
+ *         expanded characters first, then readline echos, and then print
+ *         server characters
+ *  @param c A pointer to a character
+ *  @return A boolean integer indicating whether a character was obtained
+ **/
 int get_next_char(char* c)
 {
     if (get_bs_char(c)) {
